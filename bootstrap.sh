f0db39b7a56f9cd9ab090820c278dcb608553aeb
Jennings Zhang <Jennings.Zhang@childrens.harvard.edu>
2023-12-21 14:26:55 -0500

    Configure python-chrisapp-template/bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,9 +15,9 @@
 # ----------------------------------------
 
 PLUGIN_NAME="$(basename $(dirname $(realpath $0)))"  # name of current directory
-PLUGIN_TITLE='My ChRIS Plugin'
-SCRIPT_NAME='commandname'
-DESCRIPTION='A ChRIS plugin to do something awesome'
+PLUGIN_TITLE='Zero MRI Background'
+SCRIPT_NAME='zb'
+DESCRIPTION='Set the background intensity of a MRI to 0'
 ORGANIZATION='FNNDSC'
 EMAIL='dev@babyMRI.org'
 
@@ -46,7 +46,7 @@ ENABLE_ACTIONS_BUILD=yes
 # STEP 2. Uncomment the line where it says READY=yes
 # ----------------------------------------
 
-#READY=yes
+READY=yes
 
 # ----------------------------------------
 # STEP 3. Run: ./bootstrap.sh
